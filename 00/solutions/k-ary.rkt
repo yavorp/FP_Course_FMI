947a315f81105062ab0d7dba9169fdfdd4cbdc8a
yavor <yavorp.98@gmail.com>
2019-11-03 09:17:06 +0200

    First two tasks

--- 00/solutions/k-ary.rkt
+++ 00/solutions/k-ary.rkt
@@ -1,8 +1,16 @@
 #lang racket
-
+(define (pow n k)
+    (if (= k 0) 1 
+        (* n (pow n (- k 1)))))
 (provide from-k-ary
          to-k-ary)
-
-(define (from-k-ary n k) void)
-
-(define (to-k-ary n k) void)
+(define (from-k-ary n k)
+  (define (iter i res tmp)
+    (if (= tmp 0) res
+        (iter (+ 1 i) (+ res (* (pow k i) (remainder tmp 10))) (quotient tmp 10))))
+  (iter 0 0 n))
+(define (to-k-ary n k)
+    (define (iter i res tmp)
+        (if (= tmp 0) res
+            (iter (+ 1 i) (+ res (* (pow 10 i) (remainder tmp k))) (quotient tmp k))))
+    (iter 0 0 n))
--- 00/solutions/square-root.rkt
+++ 00/solutions/square-root.rkt
@@ -2,4 +2,10 @@
 
 (provide my-sqrt)
 
-(define (my-sqrt x) void)
+(define (f x target) (- (* x x) target))
+
+(define (my-sqrt x)
+  (define (iter current i)
+    (if (= i 100) current
+        (iter (- current (/ (f current x) (* 2 current))) (+ i 1))))
+  (iter 10. 0))
