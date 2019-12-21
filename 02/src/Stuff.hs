4e6a9a61ded8138490dd7281979f0c8366e5c50a
yavor <yavorp.98@gmail.com>
2019-12-21 20:38:43 +0200

    Fixed hlint suggestions

--- 02/src/Stuff.hs
+++ 02/src/Stuff.hs
@@ -10,13 +10,14 @@ module Stuff
   ) where
 
 group :: Eq a => [a] -> [[a]]
-group = undefined
+group [] = []
+group (x:xs) = (x : takeWhile (==x) xs) : group (dropWhile (/=x) xs)
 
 -- Not mandatory, delete if you don't want this.
 insertBy :: (a -> a -> Ordering) -> a -> [a] -> [a]
-insertBy f x [] = [x]
-insertBy f x (y:ys) | (f x y) == LT  = x:(y:ys)
-                    | otherwise = y : (insertBy f x ys)
+insertBy _ x [] = [x]
+insertBy f x (y:ys) | f x y == LT  = x:(y:ys)
+                    | otherwise = y : insertBy f x ys
 
 sortBy :: (a -> a -> Ordering) -> [a] -> [a]
 sortBy _ [] = []
@@ -24,7 +25,7 @@ sortBy fn (x:xs) = insertBy fn x (sortBy fn xs)
 
 groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
 groupBy _ [] = []
-groupBy fn (x:xs) = (x : (takeWhile (\y -> fn x y) xs)) : (groupBy fn (dropWhile (\y -> fn x y) xs))
+groupBy fn (x:xs) = (x : takeWhile (fn x) xs) : groupBy fn (dropWhile (fn x) xs)
 
 on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
 on fn f x y = fn (f x) (f y)
@@ -32,33 +33,26 @@ on fn f x y = fn (f x) (f y)
 (&&&) :: (a -> b) -> (a -> c) -> a -> (b, c)
 (&&&) fn f x  = (fn x, f x)
 
--- sortOn' :: Ord b => [(a, b)] -> [(a,b)]
--- sortOn' :: [] = []
--- sortOn' :: 
-
 sortOn :: Ord b => (a -> b) -> [a] -> [a]
 sortOn _ [] = []
-sortOn fn xs = reverse (map (\x -> fst x) sortedArr)
+sortOn fn xs = reverse (map fst sortedArr)
   where
     mapedArr = map (\y -> (y, fn y)) xs
     sortedArr = sortBy (\x y -> snd y `compare` snd x) mapedArr
 
 groupOn :: Eq b => (a -> b) -> [a] -> [[a]]
 groupOn _ [] = []
-groupOn fn xs = map (\x -> (map (\y -> fst y) x)) groupedArr
+groupOn fn xs = map (map fst) groupedArr
     where
       mapedArr = map (\y -> (y, fn y)) xs
       groupedArr = groupBy (\x y -> snd x == snd y) mapedArr
 
--- helper _ [] = []
--- helper fn xs = 
-
 unique' :: Eq a => [a] -> [a]
 unique' [] = []
-unique' (x:xs) = x : (unique' (filter (\y -> y /= x) xs))
+unique' (x:xs) = x : unique' (filter (/= x) xs)
 
 classifyOn :: Ord b => (a -> b) -> [a] -> [[a]]
 classifyOn _ [] = []
-classifyOn fn xs = map (\x -> (map (\z -> fst z) (filter (\y -> snd y == x) mapedArr)))  classes
-  where classes = unique' (map (\q -> fn q) xs)
+classifyOn fn xs = map (\x -> map fst (filter (\y -> snd y == x) mapedArr))  classes
+  where classes = unique' (map fn xs)
         mapedArr = map (\x -> (x, fn x)) xs
